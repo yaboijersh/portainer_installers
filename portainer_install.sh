5c6bd702fe65a439d2fdfc837d1d7c6b92ee636d
Joshua Ross <important@joshmross.com>
2021-09-21 20:08:19 -0400

    Add files via upload

---
+++ portainer_install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+docker volume create portainer_data
+sudo docker pull portainer/portainer-ce:latest
+docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent
