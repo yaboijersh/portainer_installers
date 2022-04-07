e6f0fa1eaf222cb52b9e8cdc70b3e59525567210
Joshua Ross <important@joshmross.com>
2022-04-06 22:57:06 -0400

    Update portainer_install.sh

--- portainer_install.sh
+++ portainer_install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo docker pull portainer/portainer-ce:latest
-sudo docker run -d -p 9001:900 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo docker run -d -p 9001:9001 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
